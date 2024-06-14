06140c91fc499f861e4bcc7689416cb696dd3edb
SHUBHAM GUPTA <90739109+gshubham804@users.noreply.github.com>
2024-06-14 13:57:25 +0530

    trending search container pushed

--- src/pages/HomePage/TrendingSearch.jsx
+++ src/pages/HomePage/TrendingSearch.jsx
@@ -1,27 +1,88 @@
 
+const cardForTrendingSearch =()=>{
+    return(
+        <div className="flex justify-between">
+            <div className="flex flex-row space-x-4">
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+            <p>Total Accounts</p>
+            <p>34433434</p>
+            </div>
+            </div>
+
+            <div>
+                <p>24h</p>
+                <p>0</p>
+            </div>
+        </div>
+    )
+}
+
 const TrendingSearch = () => {
     return (
         <div className="flex justify-between">
             {/* Left Side Trending Search */}
             <div>
-                <div>
-                    <div>
-                <p>Trending Search :</p>
-                <div>
+                <div className="flex justify-between space-x-4">
+                    <div className="flex">
+                <p className="font-semibold text-xl">Trending Search :</p>
+                <div className="flex space-x-2">
                     <img src="" alt="" />
                     <p>POX</p>
                 </div>
-                <div>
+                <div className="flex space-x-2">
                     <img src="" alt="" />
                     <p>USDX</p>
                 </div>
                 </div>
-                <p>last 24 hours ago</p>
+                <p className="bg-light-red px-2 py-1">last 24 hours ago</p>
                 </div>
 
-                <div></div>
+                <div className="shadow-lg px-2 flex flex-row">
+                   <div>
+                    <cardForTrendingSearch/>
+                    <cardForTrendingSearch/>
+                   </div>
+
+                   <div>
+                   <cardForTrendingSearch/>
+                   <cardForTrendingSearch/>
+                   </div>
+                </div>
                 
-                <div></div>
+                <div className="flex justify-between px-2 py-1 shadow-lg">
+                    <div>
+                        <p>Current/MaxTPS:</p>
+                        <p>0/1000</p>
+                    </div>
+
+                    <div>
+                        <p>Nodes:</p>
+                        <p>00</p>
+                    </div>
+
+                    <div>
+                        <p>Total Tokens:</p>
+                        <p>00</p>
+                    </div>
+
+                    <div>
+                        <p>Total Contracts:</p>
+                        <p>00</p>
+                    </div>
+
+                    <div>
+                        <p>Bandwidth:</p>
+                        <p>2925033</p>
+                    </div>
+
+                    <div>
+                        <p>Energy:</p>
+                        <p>4245642</p>
+                    </div>
+                </div>
             </div>
 
             {/* Right Side Chart */}
