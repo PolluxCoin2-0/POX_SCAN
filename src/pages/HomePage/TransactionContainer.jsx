3610838cffb4b8bca065fc961b65179221945c1f
SHUBHAM GUPTA <90739109+gshubham804@users.noreply.github.com>
2024-06-14 13:38:38 +0530

    code pushed

--- src/pages/HomePage/TransactionContainer.jsx
+++ src/pages/HomePage/TransactionContainer.jsx
@@ -1,7 +1,59 @@
+import { IoIosArrowForward } from "react-icons/io";
+import { Transactions } from "../../data/HomePageData";
 
 const TransactionContainer = () => {
   return (
-    <div>TransactionContainer</div>
+    <>
+      <div className="flex justify-between">
+        <p className="font-medium text-xl">Transactions</p>
+        <p>More <IoIosArrowForward /> </p>
+      </div>
+
+      <div className="shadow-xl">
+
+        {Transactions.map((transaction, idx) => {
+          return (
+            <>
+              {/* FIrst column */}
+              <div>
+                <p>{transaction?.transactionId}</p>
+                <p>{transaction?.time} secs ago</p>
+              </div>
+
+              {/* Second column */}
+              <div>
+                <div>
+                  <p>From</p>
+                  <p>To</p>
+                </div>
+
+                <div>
+                  <p>{transaction?.from}</p>
+                  <p>{transaction?.to}</p>
+                </div>
+
+                <div>
+                  {transaction?.tags.map((tag,idx)=>{
+                    return (
+                      <>
+                      <p>{tag}</p>
+                      </>
+                    )
+                  })}
+                </div>
+              </div>
+
+              {/* Third column */}
+              <div>
+                <p>{transaction?.pox} POX</p>
+                <p className="bg-green px-2 py-1">Transfer</p>
+              </div>
+            </>
+          )
+        })}
+
+      </div>
+    </>
   )
 }
 
---
+++ src/pages/HomePage/TrendingSearch.jsx
@@ -0,0 +1,33 @@
+
+const TrendingSearch = () => {
+    return (
+        <div className="flex justify-between">
+            {/* Left Side Trending Search */}
+            <div>
+                <div>
+                    <div>
+                <p>Trending Search :</p>
+                <div>
+                    <img src="" alt="" />
+                    <p>POX</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <p>USDX</p>
+                </div>
+                </div>
+                <p>last 24 hours ago</p>
+                </div>
+
+                <div></div>
+                
+                <div></div>
+            </div>
+
+            {/* Right Side Chart */}
+            <div></div>
+        </div>
+    )
+}
+
+export default TrendingSearch
